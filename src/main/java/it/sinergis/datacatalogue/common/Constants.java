7032bc94251b0516165adabd8f1f0a1cd890e5a2
giorgiosudano <giorgio.sudano@sinergis.it>
2016-01-12 09:10:01 +0100

    added constants file

---
+++ src/main/java/it/sinergis/datacatalogue/common/Constants.java
@@ -0,0 +1,15 @@
+package it.sinergis.datacatalogue.common;
+
+public final class Constants {
+
+	public static final String ORGANIZATION_TABLE_NAME = "gsc_001_organization";
+	public static final String USER_TABLE_NAME = "gsc_002_user";
+	
+	public static final String JSON_COLUMN_NAME = "json";
+	
+	public static final String ORG_NAME_FIELD = "OrganizationName";
+	public static final String NEW_ORG_NAME_FIELD = "NewOrganizationName";
+	public static final String DESCRIPTION_FIELD = "Description";
+	public static final String USERNAME_FIELD = "Username";
+	public static final String ORGANIZATION_FIELD = "Organization";
+}
